d67b84c2a71c61628bf6e68f729f6d87e5258cea
unknown <steval_93@hotmail.com>
2017-08-31 09:17:10 -0500

    Login

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,21 @@
 'use strict';
 
-angular.module('eventosApp', ['eventosApp.constants', 'ngCookies', 'ngResource', 'ngSanitize',
-    'ui.router'
+angular.module('eventosApp', [
+  'eventosApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'satellizer'
   ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .constant("API", "http://localhost:8080")
+  .config(function($urlRouterProvider, $locationProvider,$authProvider,API) {
     $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    // Config Satellizer
+        $authProvider.loginUrl = API + '/api/auth/login';
+        $authProvider.tokenName = 'token';
+        $authProvider.tokenPrefix = 'eventosApp';
   });
